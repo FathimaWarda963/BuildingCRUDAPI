17e6bd2bc2d7d87b2be34b0e71e0abb351f42e3d
FathimaWarda <maxpluto36@gmail.com>
2026-08-19 03:51:46 +0300

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
+from typing import Optional
 
 # This creates an instance of FastAPI
 app = FastAPI(
@@ -11,8 +12,13 @@ app = FastAPI(
 class TaskCreate(BaseModel):
     title: str
 
+# This is the Pydantic model used in order to keep the structure of the tasks consistent when updating 
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    done: Optional[bool] = None
+
 # This is a miniature storage unit within the code used to demonstrate the various endpoints
-#  It acts as the database for the demonstration of this project
+# It acts as the database for the demonstration of this project
 tasks_db = [
     {"id": 1, "title": "Sand basswood strips🪵", "done": False},
     {"id": 2, "title": "Export heavy 3D models 🧊", "done": True},
@@ -69,3 +75,35 @@ def create_task(task: TaskCreate):
     # Finally, the task is added to our storage unit
     tasks_db.append(new_task)
     return new_task
+
+# This endpoint enables us to update a particular task within the storage unit using their id for search
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task_update: TaskUpdate):
+    for task in tasks_db:
+        if task["id"] == task_id:
+            # This is to ensure that the user provides the title to keep the structure consistent
+            if task_update.title is not None:
+                if not task_update.title.strip():
+                    raise HTTPException(status_code=400, detail="Title cannot be empty or blank")
+                task["title"] = task_update.title.strip()
+            
+            # This is to update the completion status of the task if provided 
+            if task_update.done is not None:
+                task["done"] = task_update.done
+                
+            return task
+
+    # If the id is not found, then an exception is raised, along with 404 error code, and an easy to understand
+    # error message
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+
+# This endpoint enables us to delete a task from the storage unit using their id for search
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: int):
+    for index, task in enumerate(tasks_db):
+        if task["id"] == task_id:
+            tasks_db.pop(index)
+            # This particular code number returns an empty body as output
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+            
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
